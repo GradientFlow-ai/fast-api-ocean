ec4b5b6fbb6377d6b4f1dc4951601cda150a88f1
Timothy Roy <timothy.roy@protonmail.ch>
2023-02-14 20:09:44 -0600

    Add new route, call API, rename service

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import json
+
+from api.t_SNE import fit_tsne
 
 app = FastAPI()
 
@@ -18,9 +21,14 @@ app.add_middleware(
 async def root():
     return {"message": "Hello from Fastapi, we are cooking now!"}
 
-@app.get("/tsne")
+@app.post("/tsne")
 async def tsne(request: Request):
-    return await request.json()
+    partially_extracted_json = await request.json()
+    target_array_as_string = partially_extracted_json['data']
+
+    # Convert string to array
+    target_array = json.loads(target_array_as_string)
+    return fit_tsne(target_array)
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8000)
